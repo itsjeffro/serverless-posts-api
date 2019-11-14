9b30af2a736549e8cc2139fd188e1c0f92f973dd
Jeffery Nielsen <jefftoddnielsen@gmail.com>
2019-11-15 07:19:55 +1100

    Set endpoints to private which will require an aws api key

--- handler.js
+++ handler.js
@@ -3,54 +3,34 @@
 module.exports.getPosts = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      [],
-      null,
-      2
-    ),
+    body: JSON.stringify([]),
   };
 }
 
 module.exports.getPost = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {},
-      null,
-      2
-    ),
+    body: JSON.stringify({}),
   };
 }
 
 module.exports.createPost = async event => {
   return {
     statusCode: 201,
-    body: JSON.stringify(
-      {},
-      null,
-      2
-    ),
+    body: JSON.stringify({}),
   };
 }
 
 module.exports.updatePost = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {},
-      null,
-      2
-    ),
+    body: JSON.stringify({}),
   };
 }
 
 module.exports.deletePost = async event => {
   return {
     statusCode: 204,
-    body: JSON.stringify(
-      null,
-      null,
-      2
-    ),
+    body: JSON.stringify(null),
   };
 }
