4045417f3083f29bcd035a8125c14f558bb0cb3b
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-17 19:27:16 +1100

    Renamed handler to index. moved posts handlers to handlers/posts

--- handlers/posts.js
+++ handlers/posts.js
@@ -3,32 +3,32 @@ module.exports.getPosts = async (event) => {
     statusCode: 200,
     body: JSON.stringify([]),
   };
-}
+};
 
 module.exports.getPost = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({}),
   };
-}
+};
 
 module.exports.createPost = async (event) => {
   return {
     statusCode: 201,
     body: JSON.stringify({}),
   };
-}
+};
 
 module.exports.updatePost = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({}),
   };
-}
+};
 
 module.exports.deletePost = async (event) => {
   return {
     statusCode: 204,
     body: JSON.stringify(null),
   };
-}
+};
