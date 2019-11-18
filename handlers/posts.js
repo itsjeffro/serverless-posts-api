040042146c4c63dfc28f5e9424c4782a77ae02bc
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-18 20:50:01 +1100

    Added get posts query to return post results

--- handlers/posts.js
+++ handlers/posts.js
@@ -1,10 +1,39 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+/**
+ * Retrive posts.
+ */
 module.exports.getPosts = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify([]),
-  };
+  connection.changeUser({ database: process.env.DB_DATABASE }, error => {
+    if (error) {
+      throw error;
+    }
+  });
+  
+  return new Promise((resolve, reject) => {
+    connection.execute("SELECT * FROM posts", (error, rows, fields) => {
+      if (error) {
+        throw error;
+      }
+      
+      resolve({
+        statusCode: 200,
+        body: JSON.stringify(rows),
+      });
+    });
+  });
 };
 
+/**
+ * Retrive a single post.
+ */
 module.exports.getPost = async (event) => {
   return {
     statusCode: 200,
@@ -12,6 +41,9 @@ module.exports.getPost = async (event) => {
   };
 };
 
+/**
+ * Create a single post.
+ */
 module.exports.createPost = async (event) => {
   return {
     statusCode: 201,
@@ -19,6 +51,9 @@ module.exports.createPost = async (event) => {
   };
 };
 
+/**
+ * Update a single post.
+ */
 module.exports.updatePost = async (event) => {
   return {
     statusCode: 200,
@@ -26,6 +61,9 @@ module.exports.updatePost = async (event) => {
   };
 };
 
+/**
+ * Delete a single post.
+ */
 module.exports.deletePost = async (event) => {
   return {
     statusCode: 204,
