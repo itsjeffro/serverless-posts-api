c8e81b30ee5f0230952f4d3aa2a513d8b5f6a83a
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-24 12:06:26 +1100

    Added Config class and test

---
+++ src/Config/index.js
@@ -0,0 +1,39 @@
+class Config {
+  constructor(items) {
+    this.items = items;
+  }
+
+  get(key, defaultValue) {
+    // If no key is provided, then return all set items.
+    if (key === undefined) {
+      return this.items;
+    }
+
+    // If the provided items has a single valid property,
+    // then return the contents of the target property.
+    if (this.items[key]) {
+      return this.items[key];
+    }
+
+    // If no multi-level key is provided and the property
+    // does not exist in items, then return a default.
+    if (key.indexOf('.') === -1) {
+      return defaultValue || null;
+    }
+
+    // If a multi-level key was provided, then we'll loop through until we get something back.
+    let items = this.items;
+
+    for (const segment of key.split('.')) {
+      if (items[segment] === undefined) {
+        return defaultValue || null;
+      }
+
+      items = items[segment];
+    }
+
+    return items;
+  }
+}
+
+module.exports = Config;
