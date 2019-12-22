941406f5371e4563a1e65a3db11c2679532042a4
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-12-22 17:29:09 +1100

    Moved logic to ObjectRepository class to reuse in other classes

--- src/lib/Config/index.ts
+++ src/lib/Config/index.ts
@@ -1,3 +1,5 @@
+import ObjectRepository from "../ObjectRepository";
+
 export default class Config {
   public items: any;
 
@@ -6,34 +8,8 @@ export default class Config {
   }
 
   public get(key?: string, defaultValue?: string) {
-    // If no key is provided, then return all set items.
-    if (key === undefined) {
-      return this.items;
-    }
+    const objectRepository = new ObjectRepository;
 
-    // If the provided items has a single valid property,
-    // then return the contents of the target property.
-    if (this.items[key]) {
-      return this.items[key];
-    }
-
-    // If no multi-level key is provided and the property
-    // does not exist in items, then return a default.
-    if (key.indexOf('.') === -1) {
-      return defaultValue || null;
-    }
-
-    // If a multi-level key was provided, then we'll loop through until we get something back.
-    let items = this.items;
-
-    for (const segment of key.split('.')) {
-      if (items[segment] === undefined) {
-        return defaultValue || null;
-      }
-
-      items = items[segment];
-    }
-
-    return items;
+    return objectRepository.get(this.items, key, defaultValue);
   }
 }
