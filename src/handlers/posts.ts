406476d39c10773d0abea830e5ce6c7971b92731
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-12-22 15:07:31 +1100

    Added update logic

--- src/handlers/posts.ts
+++ src/handlers/posts.ts
@@ -188,9 +188,54 @@ module.exports.createPost = async (event: object) => {
  * Update a single post.
  */
 module.exports.updatePost = async (event: object) => {
+  connection.changeUser({ database: process.env.DB_DATABASE }, (error: any) => {
+    if (error) {
+      throw error;
+    }
+  });
+
+  const defaults = {
+    pathParameters: {
+      uuid: null
+    },
+    body: '',
+  };
+
+  const handleEvent = Object.assign(defaults, event);
+  const postId = handleEvent.pathParameters.uuid;
+  const body = JSON.parse(handleEvent.body);
+
+  let requestData = [
+    body.title || null,
+    body.content || null,
+    new Date,
+    postId,
+  ];
+
+  let result = {
+    affectedRows: 0
+  };
+
+  result = await new Promise((resolve: any, reject: any) => {
+    connection.execute("UPDATE posts SET title = ?, content = ?, updated_at = ? WHERE id = ? LIMIT 1", requestData, (error: any, results: any, fields: any) => {
+      if (error) {
+        throw error;
+      }
+
+      resolve(results);
+    });
+  });
+
+  if (result.affectedRows === 0) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({message: `No query results for IDs: ${postId}`}),
+    };
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({}),
+    statusCode: 204,
+    body: JSON.stringify(null),
   };
 };
 
@@ -205,13 +250,6 @@ module.exports.deletePost = async (event: object) => {
   });
 
   const defaults = {
-    requestContext: {
-      authorizer: {
-        company: null,
-        user: null,
-        issuer: null
-      }
-    },
     pathParameters: {
       uuid: null
     }
