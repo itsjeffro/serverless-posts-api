d66fa5f0502436de258e6a5823ea20c3fa0919a6
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-02-15 17:43:38 +1100

    Added swagger comments

--- src/handlers/posts.ts
+++ src/handlers/posts.ts
@@ -20,7 +20,53 @@ const connection = mysql.createConnection({
 });
 
 /**
- * Get all records.
+ * @swagger
+ *
+ * definitions:
+ *   schemas:
+ *     post:
+ *       uuid: a2a8f957-c4b5-4e40-9a30-ab10cd962cbb
+ *       title: My first post
+ *       content: My post's content
+ *       created_at: 2020-02-09T04:11:57.379Z
+ *       updated_at: 2020-02-09T04:11:57.379Z
+ *
+ *   responses:
+ *      204:
+ *        description: "Returns null when successfully deleted"
+ *        example:
+ *          null
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            example:
+ *              message: "Problems parsing JSON"
+ *      404:
+ *        description: Unprocessable Entity
+ *        content:
+ *          application/json:
+ *            example:
+ *              message: "No results for table [posts] for ids: 00000000-0000-0000-0000-000000000000"
+ */
+
+/**
+ * @swagger
+ *
+ * /posts:
+ *   get:
+ *     tags:
+ *     - "posts"
+ *     summary: "Gets all posts"
+ *     description: ""
+ *     responses:
+ *       200:
+ *         description: ""
+ *         content:
+ *           application/json:
+ *             example:
+ *               -
+ *                 $ref: '#/definitions/schema/post'
  */
 module.exports.getPosts = async (event: object) => {
   const lambdaEvent = new LambdaEvent(new ObjectRepository, event);
@@ -30,7 +76,29 @@ module.exports.getPosts = async (event: object) => {
 };
 
 /**
- * Get one record.
+ * @swagger
+ *
+ * /posts/{uuid}:
+ *   get:
+ *     tags:
+ *     - "posts"
+ *     summary: "Gets one post"
+ *     description: ""
+ *     parameters:
+ *       - in: path
+ *         name: uuid
+ *         description: The UUID of the post
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: a2a8f957-c4b5-4e40-9a30-ab10cd962cbb
+ *     responses:
+ *       200:
+ *         description: ""
+ *         content:
+ *           application/json:
+ *             example:
+ *               $ref: '#/definitions/schema/post'
  */
 module.exports.getPost = async (event: object) => {
   const lambdaEvent = new LambdaEvent(new ObjectRepository, event);
@@ -40,7 +108,36 @@ module.exports.getPost = async (event: object) => {
 };
 
 /**
- * Create one record.
+ * @swagger
+ *
+ * /posts:
+ *   post:
+ *     tags:
+ *     - "posts"
+ *     summary: "Creates a posts"
+ *     description: ""
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: My first post
+ *               content:
+ *                 type: string
+ *                 example: My post's content
+ *             required:
+ *               - title
+ *     responses:
+ *       201:
+ *         description: ""
+ *         content:
+ *           application/json:
+ *             example: 
+ *               $ref: '#/definitions/schema/post'
+ *       400:
+ *         $ref: '#/definitions/responses/400'
  */
 module.exports.createPost = async (event: object) => {
   const lambdaEvent = new LambdaEvent(new ObjectRepository, event);
@@ -50,7 +147,46 @@ module.exports.createPost = async (event: object) => {
 };
 
 /**
- * Update one record.
+ * @swagger
+ *
+ * /posts/{uuid}:
+ *   put:
+ *     tags:
+ *     - "posts"
+ *     summary: "Updates a posts"
+ *     description: ""
+ *     parameters:
+ *       - in: path
+ *         name: uuid
+ *         description: The UUID of the post
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: a2a8f957-c4b5-4e40-9a30-ab10cd962cbb
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: My first post
+ *               content:
+ *                 type: string
+ *                 example: My post's content
+ *             required:
+ *               - title
+ *     responses:
+ *       200:
+ *         description: ""
+ *         content:
+ *           application/json:
+ *             example: 
+ *               $ref: '#/definitions/schema/post'
+ *       400:
+ *         $ref: '#/definitions/responses/400'
+ *       404:
+ *         $ref: '#/definitions/responses/404'
  */
 module.exports.updatePost = async (event: object) => {
   let statusCode = null;
@@ -84,7 +220,31 @@ module.exports.updatePost = async (event: object) => {
 };
 
 /**
- * Delete one record.
+ * @swagger
+ *
+ * /posts/{uuid}:
+ *   delete:
+ *     tags:
+ *     - "posts"
+ *     summary: "Deletes a posts"
+ *     description: ""
+ *     parameters:
+ *       - in: path
+ *         name: uuid
+ *         description: The UUID of the post
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: a2a8f957-c4b5-4e40-9a30-ab10cd962cbb
+ *     responses:
+ *       204:
+ *         description: ""
+ *         content:
+ *           application/json:
+ *             example: 
+ *               null
+ *       404:
+ *         $ref: '#/definitions/responses/404'
  */
 module.exports.deletePost = async (event: object) => {
   let statusCode = null;
