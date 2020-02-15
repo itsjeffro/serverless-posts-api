7c38cce23bef3c686a577b68e2296885a6775f15
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-02-15 17:42:05 +1100

    Added columns

--- migrations/20191124092254_create_posts_table.js
+++ migrations/20191124092254_create_posts_table.js
@@ -1,10 +1,13 @@
 exports.up = function(knex) {
     return knex.schema
     .createTable('posts', function (table) {
-       table.increments('id');
+       table.increments('id').unsigned();
+       table.uuid('uuid').unique().notNullable();
        table.string('title', 255).notNullable();
-       table.text('content');
+       table.string('slug', 255).notNullable();
+       table.text('content').nullable();
        table.timestamps();
+       table.timestamp('deleted_at').nullable();
     });
 };
 
