713b9e21c444b224caee4456895fe818b210a7f3
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-10-18 21:15:39 +1100

    Added temp db connection to test

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,4 +1,6 @@
-import { Response } from "express";
+import { Response, Request } from "express";
+import CreatePostService from "../services/CreatePostService";
+const mysql = require('mysql2/promise');
 
 export default class PostController {
   /**
@@ -62,17 +64,20 @@ export default class PostController {
   /**
    * Create one post.
    */
-  public create(request: any, response: Response): any {
+  public async create(request: Request, response: Response): Promise<any> {
+    const db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+
+    const createPost = new CreatePostService(db);
+
+    const data = await createPost.handle(request.body);
+
     response
       .status(201)
-      .json({
-        id: 'b48dd43d-5b19-4bce-99a8-8ffabc52db84',
-        title: 'My first post',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies enim magna, aliquet tempor nunc congue vitae. Nunc convallis elementum accumsan. Proin sit amet blandit diam. Suspendisse condimentum congue suscipit. Phasellus id sodales augue. Sed massa nibh, feugiat vitae magna a, dignissim bibendum odio.',
-        userId: 'b86bb437-eec6-4df2-9c39-800e68f84fac',
-        createdAt: '2020-11-16T18:42:29+11:00',
-        updatedAt: '2020-11-16T18:42:29+11:00',
-        deletedAt: null,
-      });
+      .json({ id: data.insertId });
   }
 }
