08720cbb73d7649d3b91a004e5cb2fb42b181b66
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-10-19 07:40:45 +1100

    Added temp logic to get posts

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,4 +1,5 @@
 import { Response, Request } from "express";
+import GetPostsService from "../services/GetPostsService";
 import CreatePostService from "../services/CreatePostService";
 const mysql = require('mysql2/promise');
 
@@ -6,26 +7,27 @@ export default class PostController {
   /**
    * List posts.
    */
-  public list(request: any, response: Response): any {
+  public async list(request: any, response: Response): Promise<any> {
+    const db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+
+    const getPosts = new GetPostsService(db);
+
+    const data = await getPosts.handle();
+
     response.json({
-      data: [
-        {
-          id: 'b48dd43d-5b19-4bce-99a8-8ffabc52db84',
-          title: 'My first post',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies enim magna, aliquet tempor nunc congue vitae. Nunc convallis elementum accumsan. Proin sit amet blandit diam. Suspendisse condimentum congue suscipit. Phasellus id sodales augue. Sed massa nibh, feugiat vitae magna a, dignissim bibendum odio.',
-          userId: 'b86bb437-eec6-4df2-9c39-800e68f84fac',
-          createdAt: '2020-11-16T18:42:29+11:00',
-          updatedAt: '2020-11-16T18:42:29+11:00',
-          deletedAt: null,
-        }
-      ],
+      data: data,
     })
   }
 
   /**
    * Show one post.
    */
-  public show(request: any, response: Response): any {
+  public async show(request: any, response: Response): Promise<any> {
     response.json({
       id: 'b48dd43d-5b19-4bce-99a8-8ffabc52db84',
       title: 'My first post',
@@ -40,7 +42,7 @@ export default class PostController {
   /**
    * Delete one post.
    */
-  public delete(request: any, response: Response): any {
+  public async delete(request: any, response: Response): Promise<any> {
     response
       .status(204)
       .json(null);
@@ -49,7 +51,7 @@ export default class PostController {
   /**
    * update one post.
    */
-  public update(request: any, response: Response): any {
+  public async update(request: any, response: Response): Promise<any> {
     response.json({
       id: 'b48dd43d-5b19-4bce-99a8-8ffabc52db84',
       title: 'My first post',
